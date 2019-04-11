26daf20af6c1c3e16bc7ac9df3c46a6ebf12f162
TheAlternateDoctor <36067537+TheAlternateDoctor@users.noreply.github.com>
2019-04-11 23:45:47 +0200

    Ajout des fonctionnalités du bouton Facebook

--- lib/Login/Login.dart
+++ lib/Login/Login.dart
@@ -3,7 +3,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import "package:firebase_auth/firebase_auth.dart";
 
-
 class Startup extends StatefulWidget {
   Startup({Key key, this.title}) : super(key: key);
   final String title;
@@ -12,18 +11,15 @@ class Startup extends StatefulWidget {
 }
 
 // Used for controlling whether the user is loggin or creating an account
-enum FormType {
-  login,
-  register
-}
+enum FormType { login, register }
 
 class _LoginPageState extends State<Startup> {
-
   final TextEditingController _emailFilter = new TextEditingController();
   final TextEditingController _passwordFilter = new TextEditingController();
   String _email = "";
   String _password = "";
-  FormType _form = FormType.login; // our default setting is to login, and we should switch to creating an account when the user chooses to
+  FormType _form = FormType
+      .login; // our default setting is to login, and we should switch to creating an account when the user chooses to
 
   _LoginPageState() {
     _emailFilter.addListener(_emailListen);
@@ -47,7 +43,7 @@ class _LoginPageState extends State<Startup> {
   }
 
   // Swap in between our two forms, registering and logging in
-  void _formChange () async {
+  void _formChange() async {
     setState(() {
       if (_form == FormType.register) {
         _form = FormType.login;
@@ -60,28 +56,26 @@ class _LoginPageState extends State<Startup> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: _buildBar(context),
-      body: new Container(
-        padding: EdgeInsets.all(16.0),
-        child: new Column(
-          children: <Widget>[
-            _buildTextFields(),
-            _buildButtons(),
-            Ink(
-            decoration: ShapeDecoration(
-            color: Colors.blue,
-            shape: CircleBorder(),
-        ),
-            child: IconButton( 
-            icon: Icon(FontAwesomeIcons.facebookF),
-            color: Colors.white,
-            tooltip: 'Connexion Facebook',
-            onPressed: () {
-              setState() {}
-            }
-        )
-      )])));}
-           
+        appBar: _buildBar(context),
+        body: new Container(
+            padding: EdgeInsets.all(16.0),
+            child: new Column(children: <Widget>[
+              _buildTextFields(),
+              _buildButtons(),
+              Ink(
+                  decoration: ShapeDecoration(
+                    color: Colors.blue,
+                    shape: RoundedRectangleBorder()
+                  ),
+                  child: IconButton(
+                      icon: Icon(FontAwesomeIcons.facebookF),
+                      color: Colors.white,
+                      tooltip: 'Connexion Facebook',
+                      onPressed: () {
+                        startFacebookLogin();
+                      }))
+            ])));
+  }
 
   Widget _buildBar(BuildContext context) {
     return new AppBar(
@@ -97,17 +91,13 @@ class _LoginPageState extends State<Startup> {
           new Container(
             child: new TextField(
               controller: _emailFilter,
-              decoration: new InputDecoration(
-                labelText: 'Email'
-              ),
+              decoration: new InputDecoration(labelText: 'Email'),
             ),
           ),
           new Container(
             child: new TextField(
               controller: _passwordFilter,
-              decoration: new InputDecoration(
-                labelText: 'Mot de passe'
-              ),
+              decoration: new InputDecoration(labelText: 'Mot de passe'),
               obscureText: true,
             ),
           )
@@ -145,7 +135,8 @@ class _LoginPageState extends State<Startup> {
               onPressed: _createAccountPressed,
             ),
             new FlatButton(
-              child: new Text('Vous avez un compte ? Cliquez ici pour vous connecter.'),
+              child: new Text(
+                  'Vous avez un compte ? Cliquez ici pour vous connecter.'),
               onPressed: _formChange,
             )
           ],
@@ -156,33 +147,47 @@ class _LoginPageState extends State<Startup> {
 
   // These functions can self contain any user auth logic required, they all have access to _email and _password
 
-  void _loginPressed () {
+  void _loginPressed() {
     print('The user wants to login with $_email and $_password');
   }
 
-  void _createAccountPressed () {
+  void _createAccountPressed() {
     print('The user wants to create an accoutn with $_email and $_password');
-
   }
 
-  void _passwordReset () {
+  void _passwordReset() {
     print("The user wants a password reset request sent to $_email");
   }
-
 }
 
-  void startFacebookLogin() async {
+void startFacebookLogin() async {
   var facebookLogin = new FacebookLogin();
-  var result = await facebookLogin.logInWithReadPermission(['email', 'public_profile']);
-    switch (result.status) {
-        case FacebookLoginStatus.loggedIn:
-          FirebaseAuth.instance.signInWithFacebook(accessToken: result.accessToken.token);
-        break;
-        case FacebookLoginStatus.cancelledByUser:
-          print("Connexion Facebook stoppe par l'utilisateur");
-        break;
-        case FacebookLoginStatus.error:
-          print("Connexion Facebook echoue");    
-        break;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  var result = await facebookLogin.logInWithReadPermissions(['email']);
+  switch (result.status) {
+    case FacebookLoginStatus.loggedIn:
+      final AuthCredential credential = FacebookAuthProvider.getCredential(
+        accessToken: result.accessToken.token,
+      );
+      final FirebaseUser user = await _auth.signInWithCredential(credential);
+      assert(user.email != null);
+      assert(user.displayName != null);
+      assert(!user.isAnonymous);
+      assert(await user.getIdToken() != null);
+
+      final FirebaseUser currentUser = await _auth.currentUser();
+      assert(user.uid == currentUser.uid);
+      if (user != null) {
+        print('Successfully signed in with Facebook. ' + user.uid);
+      } else {
+        print('Failed to sign in with Facebook. ');
       }
-    }
+      break;
+    case FacebookLoginStatus.cancelledByUser:
+      print("Connexion Facebook stoppe par l'utilisateur");
+      break;
+    case FacebookLoginStatus.error:
+      print("Connexion Facebook echoue");
+      break;
+  }
+}

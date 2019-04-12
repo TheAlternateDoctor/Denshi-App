3ae5b409fcab39aaeda1b7ea2d0f247b6c0fcbac
LucasLacrampe <l.lacrampe.jouvet@gmail.com>
2019-04-12 22:40:25 +0200

    import twitter

--- lib/Login/Login.dart
+++ lib/Login/Login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import "package:firebase_auth/firebase_auth.dart";
+import 'package:flutter_twitter_login/flutter_twitter_login.dart';
+
 
 class Startup extends StatefulWidget {
   Startup({Key key, this.title}) : super(key: key);

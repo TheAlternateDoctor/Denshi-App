216df65db29a4eb714267101735c93716223dfcf
TheAlternateDoctor <36067537+TheAlternateDoctor@users.noreply.github.com>
2019-05-04 23:37:57 +0200

    Mis à jour de global.dart pour inclure l'envoi/la recherche de variable sur la mémoire interne

--- lib/utils/global.dart
+++ lib/utils/global.dart
@@ -2,12 +2,34 @@ library denshi.globals;
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 FirebaseAuth auth = FirebaseAuth.instance;
 FirebaseUser user;
 bool isLoggedIn = false;
 Image profilepic = new Image.asset('pictures/NoPicture.png');
+String photoUrl;
 String pseudo = "void";
 String userID = "null";
 String product;
 bool isBarcode;
+String authenticationMethod = "none";
+
+void addUserToDisk(String authToken, String authToken2) async{
+SharedPreferences disk = await SharedPreferences.getInstance();
+disk.setString("pseudo",pseudo);
+disk.setString("userID",userID);
+disk.setString("authMethod",authenticationMethod);
+disk.setBool("isLoggedIn",true);
+disk.setString("photoUrl",photoUrl);
+disk.setString("authToken1", authToken);
+disk.setString("authToken2", authToken2);
+}
+void getUserFromDisk() async{
+SharedPreferences disk = await SharedPreferences.getInstance();
+pseudo = disk.getString("pseudo");
+userID = disk.getString("userID");
+authenticationMethod = disk.getString("authMethod");
+isLoggedIn = disk.getBool("isLoggedIn");
+photoUrl = disk.getString("photoUrl");
+}
